eae6e0855661873a46049d1ed97fabf20c34e2f8
dykyi.roman <mr.dukuy@gmail.com>
2025-04-30 13:33:51 +0400

    add book review

---
+++ resources/load-header.js
@@ -0,0 +1,6 @@
+fetch('../headers.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-container').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading header:', error));
