b2136c5561c8fa3e01cb7707c794d98cd95c82c1
dykyi.roman <mr.dukuy@gmail.com>
2026-03-18 09:36:34 +0100

    Add google analytics

---
+++ resources/analytics.js
@@ -0,0 +1,12 @@
+// Google Analytics (gtag.js)
+(function () {
+    var s = document.createElement('script');
+    s.async = true;
+    s.src = 'https://www.googletagmanager.com/gtag/js?id=G-Z784DG1ZCC';
+    document.head.appendChild(s);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-Z784DG1ZCC');
+})();
